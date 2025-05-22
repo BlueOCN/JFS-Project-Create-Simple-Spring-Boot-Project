c10ddce9ded9bcec3380891afc4fd7d0b140a45c
BlueOCN <robgonzalezmu@gmail.com>
2025-05-21 20:32:34 -0600

    Update - Spring Boot Project Setup

---
+++ Simple-Spring-Boot-Project/src/test/java/com/blueocn/simplespringbootproject/SimpleSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueocn.simplespringbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
