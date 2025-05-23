252905a1552522143d5eb630e6b5f7cf43b5c488
BlueOCN <robgonzalezmu@gmail.com>
2025-05-22 20:33:56 -0600

    Update - Spring Boot Scaffold done

---
+++ src/main/java/com/blueocn/JFS_Project_Create_Simple_Spring_Boot_Project/JfsProjectCreateSimpleSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.blueocn.JFS_Project_Create_Simple_Spring_Boot_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfsProjectCreateSimpleSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfsProjectCreateSimpleSpringBootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blueocn/JFS_Project_Create_Simple_Spring_Boot_Project/JfsProjectCreateSimpleSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueocn.JFS_Project_Create_Simple_Spring_Boot_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfsProjectCreateSimpleSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
