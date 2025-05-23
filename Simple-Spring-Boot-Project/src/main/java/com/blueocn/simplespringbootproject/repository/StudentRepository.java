79aaa32e06952d71f7d882fcb02eb6fcb4b40f44
BlueOCN <robgonzalezmu@gmail.com>
2025-05-22 19:45:27 -0600

    Update - Spring Boot project removed

--- Simple-Spring-Boot-Project/src/main/java/com/blueocn/simplespringbootproject/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.asdasd.simplespringbootproject.entity.Student;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-}
